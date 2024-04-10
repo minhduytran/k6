38f9d85ffa896286e65ab14a6e574a0042d0650d
Tran Duy Minh <minhtd@coin98.finance>
2024-04-10 15:43:42 +0700

    qdwqdw

--- browser2.js
+++ browser2.js
@@ -1,10 +1,25 @@
-import { group, sleep } from 'k6';
 import { browser } from 'k6/experimental/browser';
 
-export default function () {
-  group('Load homepage', function () {
-    const res = browser.get('https://example.com');
-    console.log(res.status);
-    sleep(3);
-  });
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+}
+
+export default async function () {
+  const page = browser.newPage();
+
+  try {
+    await page.goto('https://test.k6.io/');
+    // page.screenshot({ path: 'screenshots/screenshot.png' });
+  } finally {
+    page.close();
+  }
 }
